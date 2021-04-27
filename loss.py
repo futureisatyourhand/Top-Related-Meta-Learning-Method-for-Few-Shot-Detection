8a4746e250ea94cde65f0254b5bf55b7d01cffd9
Qian Li <40520052+futureisatyourhand@users.noreply.github.com>
2021-04-27 10:19:09 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,4 +1,11 @@
-#--coding:utf-8--
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2020/4/16$ 12:12$
+# @Author  : Qian Li
+# @Email   : 1844857573@qq.com
+# @File    : loss.py
+# Description :TCL-C and category-based grouping mechanism for top-metaed meta leraning method for few-shot object detection
+# --------------------------------------
 import os
 import sys
 import torch
@@ -35,29 +42,19 @@ class RankingLossFunc(nn.Module):
         #self.cuda = args.cuda
 
     def forward(self, logit, target):
-        ##logit:[batch size, number class]
-        ##target:[batch size, class]
-	#logit=softmax_loss(logit,dim=1)
-        #logit=nn.functional.softmax(logit,dim=1)
         val, ind = torch.topk(logit,2,dim=1) # top2 score
         noneOtherInd = target!=0 # not Other index
         rows = list(range(len(logit))) #row index
-        part1 = logit[rows,target] #得到每个句子真是的类别标签得分
-#         if torch.min(part1) < -20:
-#             part1 = self.gamma*(self.mPos-part1)
-#         else:
+        part1 = logit[rows,target] #
         
         #inds=(ind[:,0]==target).long()
         #print((ind[:,0]==target).shape,val.shape,inds)
-        inds =(ind[:,0]==target).long()##负样本，即为预测错误的样本
-        predF = val[rows,inds]##正样本，即为预测正确的样本
-        ###print(logit.shape,target.shape,target)
-	#div=torch.sum(torch.exp(logit),1)
+        inds =(ind[:,0]==target).long()##fase classification
+        predF = val[rows,inds]##positive classification
         
         #predF2=val[:,-1][ind[]]
 
         part1 = torch.log(1+torch.exp(self.gamma*(self.mPos-part1))) + torch.log(1+torch.exp(self.gamma*(-self.mNeg+predF)))
-        #part1 = -torch.log(self.mPos-torch.exp(part1)/div)-torch.log(-self.mNeg+torch.exp(predF)/div) # positive loss
         loss=torch.sum(part1)
         del part1,predF,logit
 
@@ -73,7 +70,8 @@ class KL_LossFunc(nn.Module):
         self.same_classes=cfg.same_classes
         print("===losss====",self.same_classes)
         self.mean_std=1.0
-        same_ids=[]##根据相似类别的分组得到每个类别对应在base类别中的id编号
+	##According to the grouping of similar categories, the ID number corresponding to each category in the Base category is obtained
+        same_ids=[]#
         for line in self.same_classes:
             n=[]       
             
@@ -124,15 +122,15 @@ class KL_LossFunc(nn.Module):
             #mean_sum[flag]#torch.exp(meta_sub.std())/torch.exp(meta_sub.std()+self.mean_std*meta_mean.std())###计算相似类别之间的均值分布差  
             #print(meta_std)
             if len(line)==1:
-                std_std[flag]=meta1_sub_std.data[line[0]]##相似类别之间的方差的方差
+                std_std[flag]=meta1_sub_std.data[line[0]]##The variance of the variances between similar categories
                 std_sum[flag]=meta1_sub_std.data[line[0]]#0.0001#torch.FloatTensor([0.00001])
-                mean_sum[flag]=meta1_sub_std.data[line[0]]##相类类别之间的均值的方差
-                mean[flag]=meta1_sub_mean.data[line[0]]##相似类别之间的均值的均值
+                mean_sum[flag]=meta1_sub_std.data[line[0]]##The variance of the mean between similar categories
+                mean[flag]=meta1_sub_mean.data[line[0]]##The mean of the means between similar categories
             else:
               
-                std_std[flag]=meta_std.std()##相似类别之间的方差的方差
-                mean_sum[flag]=meta_mean.std()##相类类别之间的均值的方差
-                mean[flag]=meta_mean.mean()##相似类别之间的均值的均值
+                std_std[flag]=meta_std.std()#
+                mean_sum[flag]=meta_mean.std()#
+                mean[flag]=meta_mean.mean()#
                 std_sum[flag]=torch.sum(meta_std)
             single_std_sum=meta_std.sum()
             flag+=1
@@ -140,20 +138,12 @@ class KL_LossFunc(nn.Module):
         std_std=Variable(std_std)
         mean_sum=Variable(mean_sum)
         std_sum=Variable(std_sum)
-        mean=Variable(mean)     
-        #meta1_sum_std_div=torch.exp(torch.sum(meta1_sub_std)).cpu() 
-        #print("==========",torch.exp(torch.sum(meta1_sub_std))) 
-        #meta1_sub_std=Variable(meta1_sub_std) 
-        #index_ids=Variable(torch.LongTensor(self.same_ids),requires_grad=False)          
-        #single_std_sum=Variable(torch.FloatTensor(single_std_sum))                   
+        mean=Variable(mean)                  
         for i in range(len(self.same_ids)):
             subloss=Variable(torch.FloatTensor([0.00005]))
             for j in range(i+1,len(self.same_ids)):
-                #subloss+=torch.exp((std_std[i]-std_std[j])**2)+self.mean_std*torch.exp((mean[i]-mean[j])**2)
                 subloss+=torch.exp((std_std[i]-std_std[j])**2)+self.mean_std*torch.exp((mean[i]-mean[j])**2)
-                 
-
-            #loss+=torch.log(1.0+1.0/(subloss+1e-6))#+\
+            
             loss+=self.weights[i]*torch.log(1.0+mean_sum[i]/(subloss+1.0/(mean_sum[i]+1e-6)))#+\
         del meta1,std_std,mean,meta1_std,meta1_mean,mean_sum#,single_std_sum
         return loss
